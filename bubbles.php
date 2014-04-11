3c1cefd012357beb6d657f6db38c3b55ba4ff4f2
Shivam Chaturvedi <devs4v@gmail.com>
2014-04-12 01:10:35 +0530

    added keys and tokens

---
+++ bubbles.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('TwitterAPIExchange.php');
+ 
+
+$settings = array(
+    'oauth_access_token' => '43135464-otST3o88CTCTTZXtAsV9WPCzdUufsbsElHeEsL9Tm',
+    'oauth_access_token_secret' => 'w5UiN3rFGvj3kq62pD66LvBQtgoNea2XzVIxUtI2GerV8',
+    'consumer_key' => '2OnxwQrJikLZI7YCx1FIHqhTU',
+    'consumer_secret' => 'jRv9QS7GEDCMojFlwsbMrCVs5gQ8IviV8ViYK86J2evMjMag6u'
+);
+
+?>
